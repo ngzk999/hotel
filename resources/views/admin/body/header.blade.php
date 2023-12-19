8b06d3a665ce0a011810d566ded5ad9a741781d3
SDPzk <n.zheng.keong@simedarbyplantation.com>
2023-12-19 22:57:52 +0800

    reflect login user in header

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -335,11 +335,13 @@
             </div>
             <div class="user-box dropdown px-3">
                 <a class="d-flex align-items-center nav-link dropdown-toggle gap-3 dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="{{asset('backend/assets/images/avatars/avatar-2.png')}}" class="user-img" alt="user avatar">
-                    <div class="user-info">
-                        <p class="user-name mb-0">Pauline Seitz</p>
-                        <p class="designattion mb-0">Web Designer</p>
-                    </div>
+                    @auth
+                        <img src="{{ empty(auth()->user()->photo) ? asset('upload/no_image.jpg') :  asset('upload/admin_images/'.auth()->user()->photo) }}" class="user-img" alt="user avatar">
+                        <div class="user-info">
+                            <p class="user-name mb-0">{{ auth()->user()->name }}</p>
+                            <p class="designattion mb-0">{{ auth()->user()->email }}</p>
+                        </div>
+                    @endauth
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end">
                     <li><a class="dropdown-item d-flex align-items-center" href="{{ route('admin.profile') }}"><i class="bx bx-user fs-5"></i><span>Profile</span></a>
