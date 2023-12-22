a933c9e51de75bd945cdb6930de190d46044aa11
ngzk999 <ngzk916@gmail.com>
2023-12-22 23:08:32 +0800

    Admin Profile Change Password

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -70,5 +71,38 @@ class AdminController extends Controller
         );
 
         return redirect()->back()->with($notification);
+    }
+
+    public function AdminPasswordChange()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_password_change', compact('profileData'));
+    }
+
+    public function AdminPasswordUpdate(Request $request)
+    {
+        // Check if the old password matches the current password
+        if (!Hash::check($request->old_password, Auth::user()->password)){
+            return redirect()->back()->withErrors(['old_password' => 'The old password does not match.']);
+        }
+
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed',
+            'new_password_confirmation' => 'required'
+        ]);
+
+        // Update password
+        Auth()->user()->update([
+            'password' => bcrypt($request->new_password)
+        ]);
+
+        $notification = array(
+            'message' => 'Password Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('admin.profile')->with($notification);
     }
 }
