6daf9a96ff449c5823bc7f02062b980a4a241c7b
SDPzk <n.zheng.keong@simedarbyplantation.com>
2023-12-28 00:31:04 +0800

    User Profile Design Part 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function Index()
     {
         return view('frontend.index');
+    }
+
+    public function UserProfile()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('frontend.dashboard.edit_profile', compact('profileData'));
     }
 }
