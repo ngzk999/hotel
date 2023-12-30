ae3101c81d2165542bf8d922d036b966143d2ad6
ngzk999 <ngzk916@gmail.com>
2023-12-30 18:45:39 +0800

    User logout option

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -54,4 +54,20 @@ class UserController extends Controller
 
         return redirect()->back()->with($notification);
     }
+
+    public function UserLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        $notification = array(
+            'message' => 'User Logout Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect('/login')->with($notification);
+    }
 }
